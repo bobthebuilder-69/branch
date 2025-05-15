9fefa25553ab2a240161d7f5b30071858d7c30a1
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-05-15 18:41:10 +0200

    j7xlte: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: samsung Model: SM-J710FN Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_j7xlte.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_j7xlte-user \
+    omni_j7xlte-userdebug \
+    omni_j7xlte-eng
---
+++ omni_j7xlte.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from j7xlte device
+$(call inherit-product, device/samsung/j7xlte/device.mk)
+
+PRODUCT_DEVICE := j7xlte
+PRODUCT_NAME := omni_j7xlte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-J710FN
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="j7xltedd-user 7.0 NRD90M J710FNDDS1BSG1 release-keys"
+
+BUILD_FINGERPRINT := samsung/j7xltedd/j7xlte:7.0/NRD90M/J710FNDDS1BSG1:user/release-keys
