dc15755b5e42566984e5ef5a8f9751b098a27ea2
bobthebuilder-69 <s.jasvndr@gmail.com>
2025-05-18 09:54:07 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_j7xlte-user
-add_lunch_combo omni_j7xlte-userdebug
-add_lunch_combo omni_j7xlte-eng
+add_lunch_combo twrp_j7xlte-user
+add_lunch_combo twrp_j7xlte-userdebug
+add_lunch_combo twrp_j7xlte-eng
